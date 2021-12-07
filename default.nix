a7afdbeb60c6f306bde4455c1af10685b280f6b5
Julien Dehos <dehos@univ-littoral.fr>
2021-12-07 01:15:14 +0100

    config + tmp

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@ pkgs.ocamlPackages.buildDunePackage {
   version = "0.1";
   minimumOCamlVersion = "4.10";
   src = ./.;
+  useDune2 = true;
   buildInputs = with pkgs.ocamlPackages; [ 
     ocamlbuild
     base
--- tmp/chap3.ml
+++ tmp/chap3.ml
@@ -31,6 +31,39 @@ let rec concat (l1 : 'a list) (l2 : 'a list) = match l1 with
     | [] -> l2
     | x :: xs -> x :: (concat xs l2);;
 
-assert ([1;2;3;4] == concat [1;2] [3;4]);;
+assert ([1;2;3;4] = concat [1;2] [3;4]);;
 
+(* Exercise: patterns *)
+
+let patt1 l = match l with
+    | "bigred" :: _ -> true
+    | _ -> false;;
+
+let patt2 l = match l with
+    | [_;_] -> true
+    | [_;_;_;_] -> true
+    | _ -> false;;
+
+let patt3 l = match l with
+    | x::y::_ -> x=y
+    | _ -> false;;
+
+assert (patt3 [1;1]);;
+assert (patt3 [1;1;2]);;
+assert (false = patt3 [1;2]);;
+assert (false = patt3 [1]);;
+
+(* Exercise: library *)
+
+let get5 (l : int list) = 
+    if List.length l < 5 then 0 else List.nth l 4;;
+
+assert (5 == get5 [1;2;3;4;5;6]);;
+assert (0 == get5 [1;2]);;
+
+let revsort (l : int list) = 
+    List.sort Stdlib.compare l 
+    |> List.rev;;
+
+assert ([4;3;2;1] = revsort [2;1;4;3]);;
 
